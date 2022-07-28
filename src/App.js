87c3cf0a7a23c1775fbe4c03b43c9f8f32dd7fcb
Alyssa Sitto <alyssasitto1@gmail.com>
2022-07-27 23:21:40 -0700

    Updated solution

--- src/App.js
+++ src/App.js
@@ -14,21 +14,21 @@ import axios from 'axios';
 function App() {
   const [countries, setCountries] = useState([]);
 
-  // useEffect(() => {
-  //   axios
-  //     .get('https://ih-countries-api.herokuapp.com/countries')
-  //     .then((countryData) => {
-  //       setCountries(countryData);
-  //     })
-  //     .catch((err) => console.log('something went wrong', err));
-  // }, []);
-
   useEffect(() => {
-    fetch('https://ih-countries-api.herokuapp.com/countries')
-      .then((res) => res.json())
-      .then((json) => setCountries(json))
+    axios
+      .get('https://ih-countries-api.herokuapp.com/countries')
+      .then((countryData) => {
+        setCountries(countryData.data);
+      })
       .catch((err) => console.log('something went wrong', err));
   }, []);
+
+  // useEffect(() => {
+  //   fetch('https://ih-countries-api.herokuapp.com/countries')
+  //     .then((res) => res.json())
+  //     .then((json) => setCountries(json))
+  //     .catch((err) => console.log('something went wrong', err));
+  // }, []);
 
   return (
     <div className="App">
